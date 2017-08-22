8193ca808ba5e6269dc7c1ec87885856a054bf3e
Marijo Maračić <marijo.maracic@gmail.com>
2017-08-22 03:11:13 +0200

    JSP and Upload controller beta.

--- src/main/java/hr/mmaracic/wsimproc/controller/ImageController.java
+++ src/main/java/hr/mmaracic/wsimproc/controller/ImageController.java
@@ -5,12 +5,26 @@
  */
 package hr.mmaracic.wsimproc.controller;
 
+import hr.mmaracic.wsimproc.model.ImageConversion;
+import hr.mmaracic.wsimproc.model.ImagePoint;
+import hr.mmaracic.wsimproc.model.User;
+import hr.mmaracic.wsimproc.service.ImageConversionService;
+import hr.mmaracic.wsimproc.service.UserService;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.imageio.ImageIO;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import static org.springframework.web.bind.annotation.RequestMethod.POST;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 /**
  *
@@ -19,14 +33,45 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 @RestController
 @RequestMapping("/image")
 public class ImageController {
-    
-    @RequestMapping(method=POST, value="/upload")
-    public String uploadImage(@RequestBody MultipartFile file, RedirectAttributes redirectAttributes){
 
-        redirectAttributes.addFlashAttribute("message",
-                "You successfully uploaded " + file.getOriginalFilename() + "!");
+    @Autowired
+    private ImageConversionService conversionService;
 
-        return "redirect:/";        
+    @Autowired
+    private UserService userService;
+
+    @RequestMapping(method = POST, value = "/upload")
+    public void uploadImage(@RequestBody MultipartFile file, HttpServletResponse response, HttpSession session) {
+
+        User user = userService.getCurrentUser();
+        List<ImagePoint> points = (List<ImagePoint>) session.getAttribute("points");
+
+        try {
+            BufferedImage image = ImageIO.read(file.getInputStream());
+            ImageConversion conversion = conversionService.performConversion(image, points, user);
+
+            session.removeAttribute("points");
+
+            response.setStatus(HttpServletResponse.SC_SEE_OTHER);
+            response.setHeader("Location", "/");
+
+            response.setContentType("image/svg+xml");
+            response.setHeader("Content-Disposition", "attachment; filename=\"output.svg\"");
+            ServletOutputStream outStr = response.getOutputStream();
+            outStr.write(conversion.getVectorImage());
+            outStr.flush();
+        } catch (IOException ex) {
+            Logger.getLogger(ImageController.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
-    
+
+    @RequestMapping(method = POST, value = "/points")
+    public void uploadPoints(@RequestBody List<ImagePoint> points, HttpServletResponse response, HttpSession session) {
+
+        session.setAttribute("points", points);
+
+        response.setStatus(HttpServletResponse.SC_SEE_OTHER);
+        response.setHeader("Location", "/");
+    }
+
 }
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -1,20 +1,26 @@
 <%@page contentType="text/html" pageEncoding="UTF-8"%>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
     "http://www.w3.org/TR/html4/loose.dtd">
 
 <html xmlns:th="http://www.thymeleaf.org">
     <body>
-        <div th:if="${message}">
-            <h2 th:text="${message}"/>
-        </div>
-
+        <!-- Points -->
+        <form method="POST" enctype="plain/text" action="/rest/image/points" id="usrform">
+            Points:<input type="text" name="points">
+            <input type="submit">
+        </form>
+        <textarea name="comment" form="usrform">Enter text here...</textarea>
+        
+        <!-- File upload -->
         <div>
-            <form method="POST" enctype="multipart/form-data" action="/image/upload">
+            <form method="POST" enctype="multipart/form-data" action="/rest/image/upload" id="imageform">
                 <table>
                     <tr><td>File to upload:</td><td><input type="file" name="file" /></td></tr>
                     <tr><td></td><td><input type="submit" value="Upload" /></td></tr>
                 </table>
             </form>
-        </div>
+        </div>        
     </body>
 </html>
